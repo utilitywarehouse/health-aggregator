632947974cc3e771115dd6e8fd563b893d59d0cb
Martin Roddam <martin.roddam@gmail.com>
2018-05-04 21:24:25 +0100

    Add support for no checks html response

--- cmd/health-aggregator/handlers.go
+++ cmd/health-aggregator/handlers.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/alecthomas/template"
+	"github.com/pkg/errors"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
@@ -144,19 +145,15 @@ func getLatestChecksForNamespace(mgoRepo *MongoRepository) http.HandlerFunc {
 			checkData.Namespace = n
 			checkData.Checks = checks
 
-			// cwd, cwdErr := os.Getwd()
-			// if cwdErr != nil {
-			// 	log.WithError(errors.Wrap(err, "failed to get current working directory"))
-			// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-			// 	w.WriteHeader(500)
-			// 	fmt.Fprint(w, "failed to get current working directory")
-			// 	return
-			// }
-
-			//tmpl, tmplErr := template.ParseFiles(filepath.Join(cwd, "templates/nschecks.html"))
+			if len(checks) == 0 {
+				w.Header().Set("Content-Type", "text/html; charset=utf-8")
+				w.WriteHeader(200)
+				fmt.Fprint(w, "No checks available")
+				return
+			}
 			tmpl, tmplErr := template.ParseFiles("/templates/nschecks.html")
 			if tmplErr != nil {
-				//log.WithError(tmplErr).Errorf(fmt.Sprintf("failed to parse file at %s", filepath.Join(cwd, "templates/nschecks.html")))
+				log.WithError(errors.Wrap(tmplErr, "failed to parse template")).Error()
 				w.Header().Set("Content-Type", "text/html; charset=utf-8")
 				w.WriteHeader(500)
 				fmt.Fprint(w, "Failed to parse template")
