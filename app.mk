bb0bd8dae7c3a504e3e7c0eb41a8b71929e6d5ff
Martin Roddam <martin.roddam@gmail.com>
2020-01-21 20:51:04 +0000

    Try the partner build image

---
+++ app.mk
@@ -0,0 +1,5 @@
+# --------------------------------------------------------------------------------------------------
+# App Variables - required for the build in CI nothing else should be in this file
+# --------------------------------------------------------------------------------------------------
+APP_NAME := health-aggregator
+APP_DESCRIPTION := Calls /__/health for services that expose the endpoint and aggregates the responses
---
+++ tools.go
@@ -0,0 +1,8 @@
+// +build tools
+
+package tools
+
+import (
+	_ "github.com/gogo/protobuf/protoc-gen-gogoslick"
+	_ "github.com/utilitywarehouse/protoc-gen-uwpartner"
+)
